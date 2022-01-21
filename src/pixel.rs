66bbcf76b979073a6b10f78ddec4424b76c23d06
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2022-01-21 08:38:44 +0200

    Make Colour fields public

--- src/pixel.rs
+++ src/pixel.rs
@@ -8,10 +8,10 @@ pub struct Bitmask {
 
 #[derive(Debug, Default, PartialEq, Clone, Copy)]
 pub struct Colour {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
 }
 
 impl Colour {
