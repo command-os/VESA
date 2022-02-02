f5d17e15e6835f287993a96efeffaa4d2562a747
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2022-02-02 18:01:20 +0200

    Add missing copyright headers

--- src/pixel.rs
+++ src/pixel.rs
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) VisualDevelopment 2021-2021.
+ * This project is licensed by the Creative Commons Attribution-NoCommercial-NoDerivatives licence.
+ */
+
 #[derive(Debug, Default, PartialEq, Clone, Copy)]
 pub struct Bitmask {
     pub r: u32,
