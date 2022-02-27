b41a9c36abe5e94c30f43b9b530677e91d9fb1e0
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2022-02-27 22:39:01 +0200

    Copyright headers use module comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-/*
- * Copyright (c) VisualDevelopment 2021-2022.
- * This project is licensed by the Creative Commons Attribution-NoCommercial-NoDerivatives licence.
- */
+//! Copyright (c) VisualDevelopment 2021-2022.
+//! This project is licensed by the Creative Commons Attribution-NoCommercial-NoDerivatives licence.
 
 #![no_std]
 #![deny(warnings, clippy::cargo, unused_extern_crates, rust_2021_compatibility)]
